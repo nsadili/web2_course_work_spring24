b9a4fb76220644025b376d3176e6a2be15e23c3f
Nuraddin Sadili <nsadili@ada.edu.az>
2024-03-06 11:40:28 +0400

    273: Add first jpa app

---
+++ 273/week 06/first_jpa_app/src/test/java/az/edu/ada/wm2/first_jpa_app/FirstJpaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.ada.wm2.first_jpa_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJpaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
